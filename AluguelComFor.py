ce7a36e778019f528cd9d41e3f4013088d59b593
Pedro Fernandes <pedrofpereira222@gmail.com>
2022-05-21 19:40:32 -0300

    Update AluguelComFor.py

--- AluguelComFor.py
+++ AluguelComFor.py
@@ -1,12 +1,10 @@
 valordiv = int(input())
 mensal = int(input())
-c = 1
-for x in range(valordiv, 0, -mensal):
-    print(f'pagamento: {c}')
+for c, x in enumerate(range(valordiv, 0, -mensal)):
+    print(f'pagamento: {c+1}')
     print(f'antes = {x}')
     if x - mensal >= 0:
         print(f'depois = {x - mensal}')
     else:
         print(f'depois = 0')
     print('-' * 5)
-    c += 1
