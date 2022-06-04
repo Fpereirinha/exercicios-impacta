cd4c5833f483062508c0288fc826f5909a2cbd03
Pedro Fernandes <pedrofpereira222@gmail.com>
2022-06-04 20:24:29 -0300

    Update listaDeCompras.py

--- listaDeCompras.py
+++ listaDeCompras.py
@@ -9,8 +9,10 @@ x = list(map(int, input().split()))
 opc = 0
 while True:
     opc = input().split()
-    if opc[0] == 'exibir':
+    if opc[0] == 'exibir' or opc[0] == "encerrar":
         exibir(x)
+        if opc[0] == "encerrar":
+            break
     elif opc[0] == 'adicionar':
         x.append(int(opc[1]))
     elif opc[0] == 'remover':
@@ -18,6 +20,3 @@ while True:
             x.remove(int(opc[1]))
         else:
             print(f'código {int(opc[1])} não encontrado')
-    elif opc[0] == 'encerrar':
-        exibir(x)
-        break
