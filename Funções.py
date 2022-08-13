bcc475ae7958d4043eb9fd71e44c54b4dc38b795
Pedro Fernandes <pedrofpereira222@gmail.com>
2022-08-12 21:08:16 -0300

    Update Funções.py

--- Funções.py
+++ Funções.py
@@ -5,7 +5,8 @@ def eh_primo(n):
                 return False
         return True
     else:
-        raise ValueError('Número deve ser maior ou igual a 2.')
+        # raise ValueError('Número deve ser maior ou igual a 2.')
+        return False
 
 def conta_primos(s):
     lista = dict()
@@ -26,40 +27,38 @@ def eh_armstrong(n):
         for c in range(tamanho):
             sum_n += int(n_str[c]) ** tamanho
     else:
-        raise ValueError('Número deve ser maior ou igual a 0 !')
+        # raise ValueError('Número deve ser maior ou igual a 0 !')
+        return False
     return sum_n == n
 
 def eh_quase_armstrong(n):
-    if n>= 0:
+    if n >= 0:
         n_str = str(n)
         tamanho = len(n_str)
         sum_n = 0
         for c in range(tamanho):
             sum_n += int(n_str[c]) ** tamanho
     else:
-        raise ValueError('Número deve ser maior ou igual a 0 !')
+        # raise ValueError('Número deve ser maior ou igual a 0 !')
+        return False
     return sum_n == n+1 or sum_n == n-1
 
 def lista_armstrong(n):
-    arms = []
-    if n >= 0:
-        for c in range(n):
-            if eh_armstrong(c):
-                arms.append(c)
-    else:
-        raise ValueError('Número deve ser maior ou igual a 0 !')
-    return sorted(arms)
+    if n < 0:
+        return []
+    return sorted([x for x in range(n) if eh_armstrong(x)])
 
 def eh_perfeito(n):
     if n >= 2:
         divisores = [numero for numero in range(1, n) if n % numero == 0]
     else:
-        raise ValueError('Numero deve ser maior ou igual a 2 !')
+        # raise ValueError('Numero deve ser maior ou igual a 2 !')
+        return False
     return sum(divisores) == n
 
 def lista_perfeitos(n):
     if n >= 2:
-        lista = [numero for numero in range(2,n) if eh_perfeito(numero)]
+        return sorted([numero for numero in range(2, n) if eh_perfeito(numero)])
     else:
-        raise ValueError('O número deve ser maior ou igual a 2 !')
-    return sorted(lista)
+        # raise ValueError('O número deve ser maior ou igual a 2 !')
+        return []
